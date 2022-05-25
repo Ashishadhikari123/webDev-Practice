9cfd19c4c8e79dd9696321144f8481820331ddaf
ASHISH ADHIKARI <56301864+Ashishadhikari123@users.noreply.github.com>
2022-05-25 11:13:43 +0530

    Create script.js

---
+++ addition of 2 numbers/script.js
@@ -0,0 +1,6 @@
+function func(){
+    var first_number = parseInt(document.getElementById("f_no").value);
+    var second_number = parseInt(document.getElementById("s_no").value);
+    var result = first_number + second_number;
+    document.getElementById("result").value = result;
+}
